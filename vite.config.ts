ca5b70b13e368976637a89faacb061cf46f90ffc
Vennila-57 <venniladevip24bcg061@skasc.ac.in>
2025-08-29 19:21:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  // Add your Vite configuration options here
-})
-
-
+  plugins: [react(), basicSsl()],
+  optimizeDeps: {
+    exclude: ["lucide-react", "better-sqlite3"],
+  },
+  define: {
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      "better-sqlite3": "better-sqlite3/lib/database.js",
+    },
+  },
+});
 
 
 
